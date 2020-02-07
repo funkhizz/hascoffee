c9591a34e4682ef9f382f2823cf9b8c3e698e767
Vladimirs Lahiza <vladimirs.lahiza@gmail.com>
2020-02-07 20:59:10 +0000

    checkout

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Cart, CartItem
 from products.models import Product
 from decimal import Decimal
+from django.contrib.auth.models import User, auth
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
@@ -28,6 +31,11 @@ def add_to_cart(request):
         cart_obj.total += product_obj.price * int(quantity)
         cart_item.save()
         cart_obj.save()
+        cart_quantity = 0
+        all_items = CartItem.objects.filter(cart=cart_obj)
+        for item in all_items:
+            cart_quantity += item.quantity
+        request.session["cart_items"] = cart_quantity
     return redirect("carts:cart")
 
 def remove_from_cart(request):
@@ -40,12 +48,18 @@ def remove_from_cart(request):
             return redirect("carts:cart")
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         cart_item, created = CartItem.objects.get_or_create(cart=cart_obj, product=product_obj)
+
         if int(quantity) == 0:
             cartitem = CartItem.objects.get(id=cart_item.id)
             cart_obj.total -= cartitem.line_total
             cartitem.cart = None
             cartitem.save()
             cart_obj.save()
+            cart_items = request.session.get("cart_items")
+            cart_items -= cartitem.quantity
+            request.session['cart_items'] = cart_items
+
+
         else:
             if cart_item.quantity > 1:
                 cart_item.quantity -= int(quantity)
@@ -54,20 +68,29 @@ def remove_from_cart(request):
                 cart_obj.total -= product_obj.price * int(quantity)
                 cart_item.save()
                 cart_obj.save()
+                cart_items = request.session.get("cart_items")
+                cart_items -= 1
+                request.session['cart_items'] = cart_items
             else:
                 cartitem = CartItem.objects.get(id=cart_item.id)
                 cartitem.cart = None
                 cart_obj.total -= product_obj.price * int(quantity)
                 cart_obj.save()
                 cartitem.save()
+                cart_items = request.session.get("cart_items")
+                cart_items -= 1
+                request.session['cart_items'] = cart_items
+
+
     return redirect("carts:cart")
 
 def checkout_home(request):
+
     cart_obj, new_obj = Cart.objects.new_or_get(request)
     cartItems = CartItem.objects.filter(cart=cart_obj.id)
     context = {
         'cart_items': cartItems,
-        'cart': cart_obj
+        'cart': cart_obj,
 
     }
     return render(request, 'checkout.html', context)
